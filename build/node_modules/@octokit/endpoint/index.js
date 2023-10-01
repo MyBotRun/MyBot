deea1828c23f2f0effc0ac7f64904ec474fda585
MonkeyHunter <>
2023-09-30 17:52:11 -0700

    Remove unneeded build folder

--- build/node_modules/@octokit/endpoint/index.js
+++
@@ -1,4 +0,0 @@
-const withDefaults = require('./with-defaults')
-const DEFAULTS = require('./lib/defaults')
-
-module.exports = withDefaults(null, DEFAULTS)
--- build/node_modules/@octokit/endpoint/lib/endpoint-with-defaults.js
+++
@@ -1,8 +0,0 @@
-module.exports = endpointWithDefaults
-
-const merge = require('./merge')
-const parse = require('./parse')
-
-function endpointWithDefaults (defaults, route, options) {
-  return parse(merge(defaults, route, options))
-}
